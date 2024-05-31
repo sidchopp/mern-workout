23cb686fdfd950a3760455e57e6a79bff1051ea7
sidchopp <sidchopp@gmail.com>
2024-05-31 11:11:00 -0600

    dispatch used in Workout Form to show updated workouts

--- client/src/components/WorkoutForm.js
+++ client/src/components/WorkoutForm.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useWorkoutsContext } from "../hooks";
 
 const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
+
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
@@ -32,7 +35,7 @@ const WorkoutForm = () => {
       setTitle("");
       setLoad("");
       setReps("");
-      console.log("new workout added:", json);
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
 
     console.log(workout);

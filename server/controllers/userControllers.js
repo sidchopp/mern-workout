dd0a4784399c0c18ab4811931cd6b1452e4a4a40
sidchopp <sidchopp@gmail.com>
2024-09-17 12:30:21 -0600

    Signup function created with hashing, installed bcrypt

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -3,14 +3,22 @@ import mongoose from "mongoose";
 import User from "../models/userModel.js";
 
 // login user
-
 const loginUser = async (req, res) => {
   res.json({ message: "login user" });
 };
 
 //signup user
 const signupUser = async (req, res) => {
-  res.json({ message: "signup user" });
+  const { email, password } = req.body;
+
+  try {
+    // Using the static signup method created in user model
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 export { loginUser, signupUser };
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,25 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  // to generate a 10 letter salt
+  const salt = await bcrypt.genSalt(10);
+
+  // Appending salt to the password before hashing
+  const hash = await bcrypt.hash(password, salt);
+
+  //hashing password, so that the user's filled password is saved as hash in DB
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 export default mongoose.model("User", userSchema);

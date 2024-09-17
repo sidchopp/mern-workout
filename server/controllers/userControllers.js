c023716e0e66ace8ff4b7ebb9a760f1dcde663f0
sidchopp <sidchopp@gmail.com>
2024-09-17 15:31:26 -0600

    Created JWT tokens for SignUp User, jsonwebtoken package installed

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 import User from "../models/userModel.js";
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
@@ -15,7 +20,10 @@ const signupUser = async (req, res) => {
     // Using the static signup method created in user model
     const user = await User.signup(email, password);
 
-    res.status(200).json({ email, user });
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }

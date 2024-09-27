c66fd7a1ad1954a154783c8c6eb510535b86347b
sidchopp <sidchopp@gmail.com>
2024-09-27 14:33:54 -0600

    Hero content updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="heroText">
       <h1>Fitness Made Simple.</h1>
-      <p>Start Now.</p>
+      <p>Create your account, add your workouts, and crush it every day!</p>
+      <Link to="/login">
+        <button>Start Now</button>
+      </Link>
     </div>
   );
 };

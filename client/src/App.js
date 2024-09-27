0420f415cc161125ceec6b63b00f82433fa54bc3
sidchopp <sidchopp@gmail.com>
2024-09-27 13:13:12 -0600

    A New hero page with image created, old hero is renamed as workouts

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,33 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useAuthContext } from "./hooks";
-import { Home, LogIn, SignUp } from "./pages";
-import { Navbar } from "./components";
-import { Footer } from "./components";
+import { Home, Workouts, LogIn, SignUp } from "./pages";
+import { Navbar, Footer } from "./components";
 
 function App() {
   const { user } = useAuthContext();
 
   return (
-    <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="app">
         <Navbar />
-        <div className="pages">
-          <Routes>
-            <Route
-              path="/"
-              element={user ? <Home /> : <Navigate to="/login" />}
-            ></Route>
-            <Route
-              path="/login"
-              element={!user ? <LogIn /> : <Navigate to="/" />}
-            ></Route>
-            <Route
-              path="/signup"
-              element={!user ? <SignUp /> : <Navigate to="/" />}
-            ></Route>
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/workouts"
+            element={user ? <Workouts /> : <Navigate to="/login" />}
+          ></Route>
+          <Route
+            path="/login"
+            element={!user ? <LogIn /> : <Navigate to="/workouts" />}
+          ></Route>
+          <Route
+            path="/signup"
+            element={!user ? <SignUp /> : <Navigate to="/workouts" />}
+          ></Route>
+        </Routes>
         <Footer />
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,52 +1,8 @@
-import { useEffect } from "react";
-import { WorkoutDetails, WorkoutForm } from "../components";
-import { useWorkoutsContext, useAuthContext } from "../hooks";
-import { URL } from "../config";
-
 const Home = () => {
-  const { workouts, dispatch } = useWorkoutsContext();
-  const { user } = useAuthContext();
-
-  useEffect(() => {
-    const fetchWorkouts = async () => {
-      try {
-        const response = await fetch(`${URL}/workouts`, {
-          credentials: "include", // Include credentials with the request
-          headers: {
-            Authorization: `Bearer ${user.token}`,
-          },
-        });
-        const json = await response.json();
-
-        if (response.ok) {
-          dispatch({ type: "SET_WORKOUTS", payload: json });
-        } else {
-          // Handle error response
-          console.error("Failed to fetch workouts:", json);
-        }
-      } catch (error) {
-        // Handle fetch error
-        console.error("Error fetching workouts:", error);
-      }
-    };
-
-    if (user) {
-      fetchWorkouts();
-    }
-  }, [dispatch, user]);
-
   return (
-    <div className="home">
-      <div className="workouts">
-        {workouts ? (
-          workouts.map((workout) => (
-            <WorkoutDetails workout={workout} key={workout?._id} />
-          ))
-        ) : (
-          <p>Loading Workouts...</p>
-        )}
-      </div>
-      <WorkoutForm />
+    <div className="heroText">
+      <h1>Fitness Made Simple</h1>
+      <p>Start Now</p>
     </div>
   );
 };
